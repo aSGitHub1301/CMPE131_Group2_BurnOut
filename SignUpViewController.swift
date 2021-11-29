9a1cee5e83358018fede5718bd54dc7863a886e5
aSGitHub1301 <95245913+aSGitHub1301@users.noreply.github.com>
2021-11-29 10:53:01 -0800

    Update SignUpViewController.swift

--- SignUpViewController.swift
+++ SignUpViewController.swift
@@ -1,9 +1,5 @@
-//
 //  SignUpViewController.swift
 //  BurnOut
-//
-//  Created by Rosey on 10/31/21.
-//
 
 import UIKit
 import FirebaseAuth
@@ -44,86 +40,3 @@ class SignUpViewController: UIViewController {
         }
     }
 }
-
-
-/*
- 
- public var completionHandler: ((String?) -> Void)?
- 
- //completionHandler?(emailInput.text)
- 
- //let settings = self.storyboard?.instantiateViewController(withIdentifier: "setting") as! SettingsViewController
- //settings.email = emailInput.text!
- 
- //let newViewController = SettingsViewController()
- //newViewController.email = emailInput.text!
- //present(newViewController, animated: true)
- 
- //signUp()
- let storyboard = UIStoryboard(name: "Main", bundle: nil)
- let vc = storyboard.instantiateViewController(withIdentifier: "setting") as! SettingsViewController
- //let vc = storyboard.instantiateViewController(withIdentifier: "setting")
- //vc.modalPresentationStyle = .overFullScreen
- vc.email = emailInput.text!
- vc.password = passwordInput.text!
- //self.tabBarController?.present(vc, animated: true, completion: nil)
- //vc.modalPresentationStyle = .fullScreen
- self.present(vc, animated: true)
- //self.tabBarController?.tabBar.isHidden = false
- 
- 
- 
- 
- 
- NotificationCenter.default.post(name: Notification.Name("text"), object: emailInput.text)
- signUp()
- let vc = storyboard?.instantiateViewController(withIdentifier: "setting") as! SettingsViewController
- vc.modalPresentationStyle = .overFullScreen
- present(vc, animated: true)
- 
- 
- 
- 
- let vc = SettingsViewController(nibName: "SettingsViewController", bundle: nil)
- vc.email = emailInput.text!
- navigationController?.pushViewController(vc, animated: true)
- 
- 
- 
- let stroyboard = UIStoryboard(name: "Main", bundle: nil)
- let vc = self.storyboard?.instantiateViewController(withIdentifier: "mainhome")
- vc?.modalPresentationStyle = .overFullScreen
- self.present(vc!, animated: true)
- 
- 
- 
- 
- let vc = storyboard?.instantiateViewController(withIdentifier: "setting") as! SettingsViewController
- vc.modalPresentationStyle = .fullScreen
- present(vc, animated: true)
- 
- 
- 
- 
- 
- if emailInput.text?.isEmpty == true{
-     print("No text in email field")
-     return
- }
- 
- if passwordInput.text?.isEmpty == true {
-     print("No text in password field")
-     return
- }
- 
- 
- 
- 
- 
- 
- NotificationCenter.default.post(name: Notification.Name("text"), object: emailInput.text)
- signUp()
- let vc = storyboard?.instantiateViewController(withIdentifier: "setting") as! SettingsViewController
- vc.modalPresentationStyle = .overFullScreen
- present(vc, animated: true)
- */
