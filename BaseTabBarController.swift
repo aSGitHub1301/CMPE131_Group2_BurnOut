f5dc3951ef1f3a51c6d7ebb3a4458f9fd53117c1
aSGitHub1301 <95245913+aSGitHub1301@users.noreply.github.com>
2021-11-29 11:00:29 -0800

    Add files via upload

---
+++ BaseTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  BaseTabBarController.swift
+//  BurnOut
+//
+//  Created by Rosey on 11/21/21.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+
+    //transfer meter value from setting to tracker page
+    var caloricIntake: Int = 2000
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
